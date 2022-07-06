18ab9c9b8348232bfa4ce33105300c264082eeb7
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2022-07-06 17:37:49 -0300

    Cadastro Reval | Criando classe de conexão com o banco.

---
+++ Service/Conexao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroReval.Service
+{
+    public class Conexao
+    {
+        SqlConnection _con = new SqlConnection();
+
+        //Construtor
+        public Conexao()
+        {
+            _con.ConnectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+        }
+
+        //Metodo Conextar
+        public SqlConnection conectar()
+        {
+            if(_con.State == System.Data.ConnectionState.Closed)
+            {
+                _con.Open();
+            }
+            
+            return _con;
+        }
+
+        public void desconectar()
+        {
+            if (_con.State == System.Data.ConnectionState.Open)
+            {
+                _con.Close();
+            }
+        }
+    }
+}
