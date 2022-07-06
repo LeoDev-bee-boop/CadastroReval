bb7c159cd520b205308ba6b95537525f65ea007b
Leonardo Bratfisch <sevalhosleonardo@gmail.com>
2022-07-05 23:39:59 -0300

    Cadastro Reval | Iniciando métodos de validação da tela de cadastro de clientes.

--- View/frmCadastraAtualizaCliente.Designer.cs
+++ View/frmCadastraAtualizaCliente.Designer.cs
@@ -31,29 +31,31 @@ namespace CadastroReval.View
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCadastraAtualizaCliente));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtCPF = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpNascimento = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbTelefone = new System.Windows.Forms.MaskedTextBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbEstadoCivil = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbRG = new System.Windows.Forms.MaskedTextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.mtbCPF = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mtbCEP = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -65,12 +67,12 @@ namespace CadastroReval.View
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome do cliente:";
             // 
-            // textBox1
+            // txtNomeCliente
             // 
-            this.textBox1.Location = new System.Drawing.Point(133, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(486, 23);
-            this.textBox1.TabIndex = 1;
+            this.txtNomeCliente.Location = new System.Drawing.Point(133, 6);
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(486, 23);
+            this.txtNomeCliente.TabIndex = 1;
             // 
             // label2
             // 
@@ -81,13 +83,6 @@ namespace CadastroReval.View
             this.label2.TabIndex = 2;
             this.label2.Text = "CPF";
             // 
-            // txtCPF
-            // 
-            this.txtCPF.Location = new System.Drawing.Point(133, 35);
-            this.txtCPF.Name = "txtCPF";
-            this.txtCPF.Size = new System.Drawing.Size(140, 23);
-            this.txtCPF.TabIndex = 3;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -97,13 +92,13 @@ namespace CadastroReval.View
             this.label5.TabIndex = 8;
             this.label5.Text = "Data de nascimento:";
             // 
-            // dateTimePicker1
+            // dtpNascimento
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(133, 94);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(81, 23);
-            this.dateTimePicker1.TabIndex = 9;
+            this.dtpNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNascimento.Location = new System.Drawing.Point(133, 94);
+            this.dtpNascimento.Name = "dtpNascimento";
+            this.dtpNascimento.Size = new System.Drawing.Size(81, 23);
+            this.dtpNascimento.TabIndex = 9;
             // 
             // label6
             // 
@@ -114,61 +109,63 @@ namespace CadastroReval.View
             this.label6.TabIndex = 10;
             this.label6.Text = "Email:";
             // 
-            // textBox4
+            // txtEmail
             // 
-            this.textBox4.Location = new System.Drawing.Point(133, 65);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(486, 23);
-            this.textBox4.TabIndex = 11;
+            this.txtEmail.Location = new System.Drawing.Point(133, 65);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(486, 23);
+            this.txtEmail.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(424, 97);
+            this.label7.Location = new System.Drawing.Point(318, 97);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(54, 15);
             this.label7.TabIndex = 12;
             this.label7.Text = "Telefone:";
             // 
-            // maskedTextBox1
+            // mtbTelefone
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(484, 94);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(135, 23);
-            this.maskedTextBox1.TabIndex = 13;
+            this.mtbTelefone.Location = new System.Drawing.Point(378, 94);
+            this.mtbTelefone.Mask = "(00) 00000-0000";
+            this.mtbTelefone.Name = "mtbTelefone";
+            this.mtbTelefone.Size = new System.Drawing.Size(82, 23);
+            this.mtbTelefone.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(220, 97);
+            this.label8.Location = new System.Drawing.Point(327, 38);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(68, 15);
             this.label8.TabIndex = 14;
             this.label8.Text = "Estado Civil";
             // 
-            // comboBox1
+            // cbEstadoCivil
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(294, 94);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(124, 23);
-            this.comboBox1.TabIndex = 15;
+            this.cbEstadoCivil.FormattingEnabled = true;
+            this.cbEstadoCivil.Location = new System.Drawing.Point(401, 35);
+            this.cbEstadoCivil.Name = "cbEstadoCivil";
+            this.cbEstadoCivil.Size = new System.Drawing.Size(218, 23);
+            this.cbEstadoCivil.TabIndex = 15;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(279, 38);
+            this.label9.Location = new System.Drawing.Point(220, 38);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(25, 15);
             this.label9.TabIndex = 16;
             this.label9.Text = "RG:";
             // 
-            // maskedTextBox2
+            // mtbRG
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(310, 35);
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(140, 23);
-            this.maskedTextBox2.TabIndex = 17;
+            this.mtbRG.Location = new System.Drawing.Point(251, 35);
+            this.mtbRG.Mask = "00,000,000-0";
+            this.mtbRG.Name = "mtbRG";
+            this.mtbRG.Size = new System.Drawing.Size(70, 23);
+            this.mtbRG.TabIndex = 17;
             // 
             // label10
             // 
@@ -197,27 +194,27 @@ namespace CadastroReval.View
             this.label12.TabIndex = 20;
             this.label12.Text = "Cidade:";
             // 
-            // textBox5
+            // txtBairro
             // 
-            this.textBox5.Location = new System.Drawing.Point(133, 123);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(126, 23);
-            this.textBox5.TabIndex = 21;
+            this.txtBairro.Location = new System.Drawing.Point(133, 123);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(126, 23);
+            this.txtBairro.TabIndex = 21;
             // 
-            // textBox6
+            // txtCidade
             // 
-            this.textBox6.Location = new System.Drawing.Point(318, 123);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(126, 23);
-            this.textBox6.TabIndex = 22;
+            this.txtCidade.Location = new System.Drawing.Point(318, 123);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(126, 23);
+            this.txtCidade.TabIndex = 22;
             // 
-            // comboBox2
+            // cbEstado
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(495, 123);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(124, 23);
-            this.comboBox2.TabIndex = 23;
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(495, 123);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(124, 23);
+            this.cbEstado.TabIndex = 23;
             // 
             // label3
             // 
@@ -236,56 +233,84 @@ namespace CadastroReval.View
             this.textBox2.Size = new System.Drawing.Size(486, 63);
             this.textBox2.TabIndex = 25;
             // 
-            // button1
+            // btnGravar
             // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(552, 222);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 23);
-            this.button1.TabIndex = 26;
-            this.button1.Text = "Gravar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnGravar.BackColor = System.Drawing.Color.Transparent;
+            this.btnGravar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGravar.Location = new System.Drawing.Point(552, 222);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(67, 23);
+            this.btnGravar.TabIndex = 26;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = false;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
             // 
-            // button2
+            // btnCancelar
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.Location = new System.Drawing.Point(479, 222);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(67, 23);
-            this.button2.TabIndex = 27;
-            this.button2.Text = "Cancelar";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnCancelar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCancelar.Location = new System.Drawing.Point(479, 222);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(67, 23);
+            this.btnCancelar.TabIndex = 27;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // mtbCPF
+            // 
+            this.mtbCPF.Location = new System.Drawing.Point(133, 35);
+            this.mtbCPF.Mask = "000,000,000-00";
+            this.mtbCPF.Name = "mtbCPF";
+            this.mtbCPF.Size = new System.Drawing.Size(81, 23);
+            this.mtbCPF.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 15);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "CEP";
+            // 
+            // mtbCEP
+            // 
+            this.mtbCEP.Location = new System.Drawing.Point(254, 94);
+            this.mtbCEP.Mask = "00000-000";
+            this.mtbCEP.Name = "mtbCEP";
+            this.mtbCEP.Size = new System.Drawing.Size(58, 23);
+            this.mtbCEP.TabIndex = 30;
             // 
             // frmCadastraAtualizaCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(631, 252);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mtbCEP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mtbCPF);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.cbEstado);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.mtbRG);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbEstadoCivil);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.mtbTelefone);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dtpNascimento);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtCPF);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNomeCliente);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -294,6 +319,7 @@ namespace CadastroReval.View
             this.Name = "frmCadastraAtualizaCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de clientes";
+            this.Load += new System.EventHandler(this.frmCadastraAtualizaCliente_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -302,28 +328,30 @@ namespace CadastroReval.View
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNomeCliente;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtCPF;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpNascimento;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox mtbTelefone;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbEstadoCivil;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox mtbRG;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.ComboBox cbEstado;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.MaskedTextBox mtbCPF;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox mtbCEP;
     }
 }
--- View/frmCadastraAtualizaCliente.cs
+++ View/frmCadastraAtualizaCliente.cs
@@ -16,5 +16,89 @@ namespace CadastroReval.View
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmCadastraAtualizaCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            gravarCliente();
+        }
+
+        private void gravarCliente()
+        {
+            if (validaDadosCliente() == false)
+                return;
+
+            atribuiDadosCliente();
+        }
+
+        private void atribuiDadosCliente()
+        {
+
+        }
+
+        private bool validaDadosCliente()
+        {
+            if (string.IsNullOrEmpty(txtNomeCliente.Text))
+            {
+                MessageBox.Show("Insira o nome do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(mtbCPF.Text))
+            {
+                MessageBox.Show("Insira o CPF do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Insira o email do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(mtbCEP.Text))
+            {
+                MessageBox.Show("Insira o CEP do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(mtbTelefone.Text))
+            {
+                MessageBox.Show("Insira o telefone do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtBairro.Text))
+            {
+                MessageBox.Show("Insira o bairro do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (string.IsNullOrEmpty(txtCidade.Text))
+            {
+                MessageBox.Show("Insira a cidade do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (cbEstado.SelectedIndex == 0)
+            {
+                MessageBox.Show("Insira o estado do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (cbEstadoCivil.SelectedIndex == 0)
+            {
+                MessageBox.Show("Insira o estado civil do cliente!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            return true;
+        }
     }
 }
